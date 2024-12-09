1b0d88f6f5f7b0b11909b3eba266a09825033b39
Akshaya Rajarajan <akshaya.rajarajan.96@gmail.com>
2024-12-09 01:30:50 -0600

    added the payment calculation and search bar features

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -1,64 +1,93 @@
 import {cart, addToCart} from "../data/cart.js";
 import {products} from '../data/products.js';
 
-let productsHTML = '';
+const productsGrid = document.querySelector('.js-products-grid');
+const searchText = document.querySelector('.js-search-bar')
+const searchButton = document.querySelector('.js-search-button')
+
+function displayProducts(selectedProducts){
+  let productsHTML = '';
 // product_len = products.length;
 // console.log(product_len);
-products.forEach((product) => {
-  productsHTML += `
-    <div class="product-container">
-      <div class="product-image-container">
-        <img class="product-image"
-          src="${product.image}">
-      </div>
-
-      <div class="product-name limit-text-to-2-lines">
-        ${product.name}
-      </div>
-
-      <div class="product-rating-container">
-        <img class="product-rating-stars"
-          src="images/ratings/rating-${product.rating.stars * 10}.png">
-        <div class="product-rating-count link-primary">
-          ${product.rating.count}
+  selectedProducts.forEach((product) => {
+    productsHTML += `
+      <div class="product-container">
+        <div class="product-image-container">
+          <img class="product-image"
+            src="${product.image}">
         </div>
+
+        <div class="product-name limit-text-to-2-lines">
+          ${product.name}
+        </div>
+
+        <div class="product-rating-container">
+          <img class="product-rating-stars"
+            src="images/ratings/rating-${product.rating.stars * 10}.png">
+          <div class="product-rating-count link-primary">
+            ${product.rating.count}
+          </div>
+        </div>
+
+        <div class="product-price">
+          $${(product.priceCents / 100).toFixed(2)}
+        </div>
+
+        <div class="product-spacer"></div>
+
+        <div class="added-to-cart">
+          <img src="images/icons/checkmark.png">
+          Added
+        </div>
+
+        <button class="add-to-cart-button button-primary js-add-to-cart"
+        data-product-id="${product.id}">
+          Add to Cart
+        </button>
       </div>
-
-      <div class="product-price">
-        $${(product.priceCents / 100).toFixed(2)}
-      </div>
-
-      <div class="product-quantity-container">
-        <select>
-          <option selected value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9</option>
-          <option value="10">10</option>
-        </select>
-      </div>
-
-      <div class="product-spacer"></div>
-
-      <div class="added-to-cart">
-        <img src="images/icons/checkmark.png">
-        Added
-      </div>
-
-      <button class="add-to-cart-button button-primary js-add-to-cart"
-      data-product-id="${product.id}">
-        Add to Cart
-      </button>
-    </div>
   `;
 });
+  productsGrid.innerHTML = productsHTML;
 
-document.querySelector('.js-products-grid').innerHTML = productsHTML;
+  document.querySelectorAll('.js-add-to-cart')
+  .forEach((button) => {
+    button.addEventListener('click', () => {
+      const productId = button.dataset.productId;
+
+      addToCart(productId);
+      updateCartQuantity();
+
+    });
+  });
+}
+
+displayProducts(products);
+
+searchButton.addEventListener('click', () => {
+  const text = searchText.value.trim().toLowerCase();
+  let selectedProducts = [];
+
+  if(text){
+    products.forEach((product) => {
+      let matchProd = false;
+
+      product.keywords.forEach((keyword) => {
+        if (keyword.toLowerCase() === text) {
+          matchProd = true;
+        }
+      });
+      
+      if(matchProd){
+        selectedProducts.push(product);
+      }
+    });
+  }
+  else{
+    selectedProducts = products;
+  }
+  displayProducts(selectedProducts);
+});
+
 
 function updateCartQuantity(){
   
@@ -73,13 +102,3 @@ function updateCartQuantity(){
   
 }
 
-document.querySelectorAll('.js-add-to-cart')
-  .forEach((button) => {
-    button.addEventListener('click', () => {
-      const productId = button.dataset.productId;
-
-      addToCart(productId);
-      updateCartQuantity();
-
-    });
-  });
